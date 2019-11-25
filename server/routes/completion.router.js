5a49fee5297cab6c1fe87e8c000b2adf5d0a2691
Grant Krieger <fgkrieger13@gmail.com>
2019-11-25 10:57:27 -0600

    succesfully fills completions table at midnight for each day

--- server/routes/completion.router.js
+++ server/routes/completion.router.js
@@ -5,44 +5,44 @@ const router = express.Router();
 
 
 //USE CRON TO POPULATE COMPLETION TABLE
-  
+
 const CronJob = require('../../node_modules/cron/lib/cron').CronJob;
 
 console.log('Before job instantiation');
-const job = new CronJob('00 18 10 * * *', function() {
+const job = new CronJob('00 01 00 * * *', function () {
     const d = new Date();
     let day = d.getDay();
     console.log('Midnight:', day);
-    // if (req.body.sunday = true && d === 1){
-    //     return router.post('/', rejectUnauthenticated, (req, res) => {})
-    // }
-    if (day === 1){
-        return router.post('/', (req, res) => {
-            const queryText = `INSERT INTO "completion" (
-                "bets_id",
-                "status",
-              ) VALUES ($1, $2);
-                `
-            const queryValues = [
-                bets_id = req.body.bets_id,
-                status = false,
-            ]
 
-            pool.query(queryText, queryValues)
-        .then(result => {
-            res.sendStatus(200)
-        }).catch(error => {
-            console.log('error in completion post', error)
-            res.sendStatus(500)
-        })
+    if (day === 1) {
+        const queryText = `SELECT "bets"."id" as bets_id, * FROM "bets"
+            JOIN "bet_type" ON "bets"."bet_type_id" = "bet_type"."id"
+            WHERE "bets"."monday" = 'true'
+            ORDER BY "bets"."id" DESC
+            ;`
 
-        })
+        pool.query(queryText)
+            .then(result => {
+                console.log(result.rows)
+                for (bet of result.rows) {
+                const queryTextTwo = `INSERT INTO "completion" ("bets_id", "status", "time") 
+            VALUES ($1, $2, $3)`
+
+                pool.query(queryTextTwo, [bet.bets_id, false, d])
+                    .then(result => {
+                        console.log(result.rows)
+                    
+                    }).catch(error => {
+                        console.log('error in completion post', error)
+                    })
+                }
+            }).catch(error => {
+                console.log('error in completion post', error)
+            })
+
     }
-    // if (req.body.tuesday = true && d === 2){
-    //     return router.post('/', rejectUnauthenticated, (req, res) => {
-    //     })
-    // }
 });
+
 console.log('After job instantiation');
 job.start();
 
